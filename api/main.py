be5a0a3d9e8c74b23ae1b58db0ebb03b7abb5414
Deepak <deepakpadukone20@live.com>
2025-07-01 19:56:00 +0530

    removed uvicorn from main.py

--- api/main.py
+++ api/main.py
@@ -114,6 +114,3 @@ def update_user(user_id: UUID, updated_user: User):
 async def root():
     return {"Hello": "World"}
 
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=8000))
